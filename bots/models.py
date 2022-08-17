57beab4a4568dc9d622a68e96fb521ada6458b8f
Gabriel Schneider <gabrielmunizsch@gmail.com>
2022-08-17 16:31:32 -0300

    deploy heroku

--- bots/models.py
+++ bots/models.py
@@ -1,14 +1,7 @@
 from django.db import models
 
 class Bot(models.Model):
-     description = models.TextField()
-     hdd = models.TextField()
-     hdd_unavailable = models.TextField()
-     product_id = models.CharField(max_length=20)
-     price = models.CharField(max_length=20)
-     reviews = models.CharField(max_length=50)
-     starts = models.IntegerField()
-     product_infos = models.CharField(max_length=256)
+
      
      ...
   
--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = "django-insecure-*=lqiu5v8qcqlr=qg6%db3o$zt*_pr-lsyxdp^7ax@69funnwa"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
